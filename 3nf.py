a8c71f63c17406a9eae1ff04c01503a5a81968f1
ahnguyen03 <ahnguyen@ualberta.ca>
2016-11-22 10:18:27 -0700

    j

--- 3nf.py
+++ 3nf.py
@@ -1,16 +1,23 @@
 T = [["ABH","CK"],["A","D"],["C","E"],["BGH","F"],["F","AD"],["E","F"],["BH","E"]]
-print(T)
-print(len(T[1]))
-k = T[0]
-print(k[1][1])
+
+#===============================================================================
+# print(T)
+# print(len(T[1]))
+# k = T[0]
+# print(k[1][1])
+#===============================================================================
 
 
 def minimal_cover(Fundalmental_dep):
     Temp_Fundalmental = []
+    Clousure_Fundalmental_dep = []
+    for closure_FD in Fundalmental_dep:
+        print(closure_FD[0]) 
+        #Closure_Fundalmental_dep.append(closure_FD[0])
     for FD in Fundalmental_dep:
             for i in range(0,len(FD[1])):
                 Temp_Fundalmental.append((FD[0],FD[1][i]))
-    print(Temp_Fundalmental)
-
+    #print(Temp_Fundalmental)
+    
 minimal_cover(T)
 
