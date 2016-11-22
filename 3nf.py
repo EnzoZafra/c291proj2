aeccc910ecdf11fcd20d69f101cda2430839a988
ahnguyen03 <ahnguyen@ualberta.ca>
2016-11-21 21:35:32 -0700

    Revert "ynhtgbfgbgfbg"
    This reverts commit e4f86ab46219af35f6970f6893647068be384d53.

--- 3nf.py
+++ 3nf.py
@@ -1,15 +1,1 @@
-T = [["ABH","CK"],["A","D"],["C","E"],["BGH","F"],["F","AD"],["E","F"],["BH","E"]]
-print(T)
-print(len(T[1]))
-k = T[0]
-print(k[1][1])
-
-
-def minimal_cover(Fundalmental_dep):
-    Temp_Fundalmental = []
-    for FD in Fundalmental_dep:
-            for i in range(0,len(FD[1])):
-                Temp_Fundalmental.append((FD[0],FD[1][i]))
-    print(Temp_Fundalmental)
-
-minimal_cover(T)
+import sqlite3
