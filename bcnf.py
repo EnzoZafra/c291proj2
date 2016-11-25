070972144c73d8bb0eb0a7d18431fcd6c72bc8e0
choang1 <charles.hoang11@gmail.com>
2016-11-24 21:58:00 -0700

    fome

--- bcnf.py
+++ bcnf.py
@@ -2,86 +2,73 @@ from helper import *
 import main
 import sqlcontroller as controller
 
-#attribute_list = ['A','B']
-   # combined_fds = [ [['A','B'],['C']], [['A'],['D']], [['D'],['E']],[['A','C'],['B']] ]
-   #computeClosure(attribute_list, combined_fds):
-
 
 #===============================================================================
-# This finds if it is SUPER KEY
-#  for atr in Z:
-#      print(computeClosure(atr[0],Z))
-#      print(set(computeClosure(atr[0], Z)).issuperset(set(Zatt)))
+# #This function checks if the attribute list is in BCNF form
 #===============================================================================
-
-
-
-# isBCNF(["AB"] [all funcdeps])
 def isBCNF(attrList, funcDep, atr):
     
     if (set(computeClosure(atr,funcDep)).issuperset(attrList)):
         return True
     return False
 
-def isDependencyPreserved():
-    return -1
+def isSuperKey(attributeList, funcDep, attrList):
+    return set(computeClosure(attributeList,funcDep)).issuperset(attrList)
 
+#===============================================================================
+# #Function used to decompose the schema
+# #attrList is the list of all the attributes
+# #funcDep is the all the functional dependencies for this relation
+#===============================================================================
 def BCNT(attrList, funcDep):
 #     print(isBCNF(attrList,funcDep))
+
+    #Search for all the super keys
     superKey = []
-    for atr in funcDep:
-        if (set(computeClosure(atr[0],funcDep)).issuperset(attrList)):
-            superKey.append(atr[0])
-#     print("super keys are: ",(superKey))
+    for FD in funcDep:
+        if (isSuperKey(FD[0],funcDep,attrList)):
+            superKey.append(FD[0])
     
-    
-    #===========================================================================
-    # find minimal key
-    #  if superKey[0] != []:
-    #      minimalKey = superKey[0]
-    #      length = len(superKey[0])
-    #      for key in superKey:
-    #          if len(key) <= length:
-    #              minimalKey = key
-    #      print("Minimal Key is ", minimalKey)
-    #===========================================================================
-    
+    #Variable to hold the attribute Lists R1,R2,R3
     relation = []
     relation.append(attrList)
-    
+    #Temporary variable for funcDep that gets customized
     functDepend = list(funcDep)
+    #Variable to store the FDs of the split relations
     newFDs = []
+    #Dictionary used to put into Relation form
     R = {}
     
+    #Counter for R1,R2,R3 leaving the first index the original that loses attributes
     splitCount = 1
-#     print("Initial Relation: ", attrList)
-#     print("Initial FDs: ", funcDep)
-    for atr in funcDep:
-#         print("Atr: ", atr)
-        if atr in functDepend:
-            newFDs.append(atr)
-            if not (isBCNF(attrList, funcDep, atr[0])):
+
+    for fd in funcDep:
+        
+        if fd in functDepend:
+            
+            #If the LHS of FD is not in BCNF, split
+            if not (isBCNF(attrList, funcDep, fd[0])):
+                newFDs.append(fd)
+                
                 relation.append([])
-#                 print("LHS: ", atr[0])
-                for Attribute in atr[0]:
+                #Add the attributes on LHS to Ri
+                for Attribute in fd[0]:
                     relation[splitCount].append(Attribute)
-#                 print("RHS: ", atr[1])
-                for Attribute in atr[1]:
+                #Add attributes on RHS to Ri and remove attributes from original
+                for Attribute in fd[1]:
                     relation[0].remove(Attribute)
                     relation[splitCount].append(Attribute)
-                    
-                functDepend.remove(atr)
+                #Removed this FD
+                functDepend.remove(fd)
+                #Remove the removed attributes from remaining FDs
                 for FD in functDepend:
-                    for attriRemoved in atr[1]:
+                    for attriRemoved in fd[1]:
                         if attriRemoved in FD[1]:
-#                             print("at remove: ", attriRemoved)
-#                             print(FD[1])
+
                             FD[1].remove(attriRemoved)
-#                 print("Relation split: ",relation)    
-#                 print("Remaining FDs: ", functDepend)
+                #increment counter
                 splitCount += 1
-    count = -1
-#     print("NEW FDS: " , newFDs)
+    #To put relation and FD together as a dictionary
     for table in relation:
         for FD in newFDs:
             tempList = []
@@ -97,8 +84,14 @@ def BCNT(attrList, funcDep):
         if not R.get("".join(table),[]):
 
             R["".join(table)] = []
+    print("orignial:", funcDep)
+#     for FD in newFDs:
+#         print(FD)
+    print(newFDs)
+    print(checkEqual(funcDep,newFDs))
     print(R)
     
+     
 R = ['A','B','C','D','E','F','G','H']
 TR = [[['A','B','H'],['C']], [['A'],['D','E']],[['B','G','H'],['F']],[['F'],['A','D','H']],[['B','H'],['G','E']]]
 Z = [[['A','B'],['C']], [['A'],['D']], [['D'],['E']],[['A','C'],['B']]]
@@ -106,7 +99,6 @@ Z = [[['A','B'],['C']], [['A'],['D']], [['D'],['E']],[['A','C'],['B']]]
 Zatt = ['A','B',"C",'D','E']
 T = [["ABH","CK"],["A","D"],["C","E"],["BGH","F"],["F","AD"],["E","F"],["BH","E"]]
 BCNT(R,TR)
-print()
 BCNT(Zatt,Z)    
 Bank = ['B','C','A','N','L','M']
 BankFD = [[['B'],['A','C']],[['L'],['M','B']]]
@@ -114,9 +106,13 @@ BCNT(Bank,BankFD)
 R = ['A','B','C','D','E','F','G','H','K']
 T = [[['A', 'B', 'H'], ['C', 'K']], [['A'], ['D']], [['C'], ['E']], [['B', 'G', 'H'], ['F']], [['F'], ['A', 'D']], [['E'], ['F']], [['B', 'H'], ['E']]]
 T1 = [[['B','H'],['E']],[['A', 'B', 'H'], ['C', 'K']], [['A'],['D']], [['E'],['F']],[['F'],['A']]]
+print("test",checkEqual(T,T1))
 BCNT(R,T)
 # print(checkEqual(T,T1))
 R = ['A','B','C','D']
 T = [[['A'],['B']],[['B'],['C']]]
 BCNT(R,T)
-# print(checkEqual(T,T))
+print(checkEqual(T,T))
+R = ['A','B','C','F','G','H']
+T = [[['A','B','H'],['C']],[['B','G','H'],['F']],[['F'],['A','H']],[['B','H'],['G']]]
+BCNT(R,T)
